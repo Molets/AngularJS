26f3bf57d637df3acc90dde224be8ac1bd735ec3
Molets <musomoletsane@gmail.com>
2024-05-08 13:06:30 +0200

    Final commit

--- content/scripts/employees.js
+++ content/scripts/employees.js
@@ -1,49 +1,71 @@
 var employeesApp = angular.module('employeesApp', ['ngRoute']);
 
-employeesApp.config(['$routeProvider', function($routeProvider){
+employeesApp.config(['$routeProvider', '$locationProvider',
+                    function($routeProvider, $locationProvider){
+
+  $locationProvider.html5Mode(true);
 
   $routeProvider
     .when('/home', {
-      templateUrl : 'content/views/home.html'
+      templateUrl : 'content/views/home.html',
+      controller : 'EmployeesController'
     })
-    .when('/employees', {
+    .when('/list', {
       templateUrl : 'content/views/list.html',
       controller : 'EmployeesController'
     })
     .otherwise({
       redirectTo : '/home'
-    })
+    });
 
 }]);
 
-employeesApp.controller('EmployeesController',['$scope', function($scope){
+employeesApp.directive('randomEmployee',[function(){
 
-  $scope.employees = [
-    {
-      name : "Palesa",
-      age : 34,
-      salary : 3000,
-      available : true
+  return {
+    restrict : 'E',
+    scope : {
+      employees : '=',
+      title : '='
     },
-    {
-      name : "Lerato",
-      age : 27,
-      salary : 2500,
-      available : false
-    },
-    {
-      name : "Liteboho",
-      age : 41,
-      salary : 5000,
-      available : true
-    },
-    {
-      name : "Lenka",
-      age : 23,
-      salary : 3800,
-      available : true
+    templateUrl : 'content/views/random_employee.html',
+    replace : true,
+    transclude : true,
+    controller : function($scope){
+      $scope.random = Math.floor(Math.random() * 4);
+      console.log($scope.random);
     }
-  ];
+  };
+
+}]);
+
+employeesApp.controller('EmployeesController',['$scope', '$http', function($scope, $http){
+
+  $http.get('data/employees.json')
+        .then(function(response){
+
+          $scope.employees = response.data;
+
+        }, function(error){
+
+          console.log(error);
+
+        });
+
+  $scope.addEmployee = ()=>{
+
+    $scope.employees.push({
+      name : $scope.newEmployee.name,
+      age : $scope.newEmployee.age,
+      salary : $scope.newEmployee.salary,
+      available : true
+    });
+
+    $scope.newEmployee.name = '';
+    $scope.newEmployee.age = '';
+    $scope.newEmployee.salary = '';
+
+  };
 
   $scope.deleteEmployee = function(employee){
 
